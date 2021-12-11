7e76dfb782c404d5dcafcdafda11c23d3acd647d
linhnmfx12988 <linhnmfx12988@funix.edu.vn>
2021-12-11 17:58:56 +0700

    asm03 loading 45%

--- src/components/NhanvienComponent.js
+++ src/components/NhanvienComponent.js
@@ -1,21 +1,33 @@
 import React, { Component } from "react";
-import { Card, CardImg, CardTitle, Button, Modal, ModalBody, ModalHeader, Label, Row, Col } from "reactstrap";
-import { Link } from 'react-router-dom';
+import {
+  Card,
+  CardImg,
+  CardTitle,
+  Button,
+  Modal,
+  ModalBody,
+  ModalHeader,
+  Label,
+  Row,
+  Col,
+} from "reactstrap";
+import { Link } from "react-router-dom";
 import { STAFFS } from "../shared/staffs";
 import { Control, LocalForm, Errors } from "react-redux-form";
 
 ///// Validators
 const required = (val) => val && val.length;
-const minLength = (len) => (val) => (val) && (val.length >= len);
+const length = (val) => val && val.length === "";
+const maxLength = (len) => (val) => !(val) || (val.length <= len);
+const minLength = (len) => (val) => val && val.length >= len;
 
 class Nhanvien extends Component {
-
   constructor(props) {
     super(props);
 
     this.state = {
       staffs: STAFFS,
-      isAddFormModalOpen: false
+      isAddFormModalOpen: false,
     };
     this.handelSearch = this.handelSearch.bind(this);
     this.input = React.createRef();
@@ -27,7 +39,11 @@ class Nhanvien extends Component {
   //--------------Uncontrolled Form----------------
   handelSearch(event) {
     this.setState({
-      staffs: this.props.staffs.filter(staff => staff.name.toLowerCase().includes(this.input.current.value.toLowerCase()))
+      staffs: this.props.staffs.filter((staff) =>
+        staff.name
+          .toLowerCase()
+          .includes(this.input.current.value.toLowerCase())
+      ),
     });
     event.preventDefault();
   }
@@ -40,25 +56,28 @@ class Nhanvien extends Component {
 
   toggleAddFormModal() {
     this.setState({
-      isAddFormModalOpen: !this.state.isAddFormModalOpen
+      isAddFormModalOpen: !this.state.isAddFormModalOpen,
     });
   }
 
   render() {
-
-    const RenderNVItem = ({ staff }) =>
+    const RenderNVItem = ({ staff }) => (
       <Card style={{ border: "1px solid rgb(112, 112, 112)" }}>
-        <Link to={`/nhanvien/${staff.id}`} >
+        <Link to={`/nhanvien/${staff.id}`}>
           <CardImg width="100%" src={staff.image} />
-          <CardTitle className="text-center" style={{ color: "black" }}> {staff.name}</CardTitle>
+          <CardTitle className="text-center" style={{ color: "black" }}>
+            {" "}
+            {staff.name}
+          </CardTitle>
         </Link>
       </Card>
+    );
 
-    const nvien = this.state.staffs.map((staff) =>
+    const nvien = this.state.staffs.map((staff) => (
       <div key={staff.id} className="col-6 col-md-4 col-lg-2">
         <RenderNVItem staff={staff} />
       </div>
-    );
+    ));
 
     return (
       <div className="container">
@@ -68,7 +87,6 @@ class Nhanvien extends Component {
           <div className="col">
             <nav className="navbar navbar-light bg-light">
               <div className="container-fluid">
-
                 {/*************************Button Add***************************/}
 
                 <Button outline onClick={this.toggleAddFormModal}>
@@ -76,30 +94,32 @@ class Nhanvien extends Component {
                 </Button>
 
                 {/*Add Form Modal*/}
-                <Modal isOpen={this.state.isAddFormModalOpen}
+                <Modal
+                  isOpen={this.state.isAddFormModalOpen}
                   toggle={this.toggleAddFormModal}
                 >
-                  <ModalHeader toggle={this.toggleAddFormModal}>
-                    {" "}Thêm Nhân Viên{" "}
+                  <ModalHeader toggle={this.toggleAddFormModal}>                    
+                    Thêm Nhân Viên
                   </ModalHeader>
                   <ModalBody>
                     <LocalForm
                       onSubmit={(values) => this.handelAddFormSubmit(values)}
                     >
                       <Row className="form-group">
-                        <Label htmlFor="name" md={4}>
-                          {" "}Tên:{" "}
+                        <Label htmlFor="name" md={5}>                         
+                          Tên
                         </Label>
-                        <Col md={8}>
+                        <Col md={7}>
                           <Control.text
                             model=".name"
                             id="name"
                             name="name"
-                            placeholder="Họ tên ..."
                             className="form-control"
                             validators={{
                               required,
-                              minLength: minLength(1),
+                              length: length(""),
+                              maxLength: maxLength(30),
+                              minLength: minLength(2),
                             }}
                           />
                           <Errors
@@ -108,23 +128,25 @@ class Nhanvien extends Component {
                             show="touched"
                             messages={{
                               require: "Required",
-                              minLength: "Yêu cầu nhập",
+                              length: "Yêu cầu nhập",
+                              maxLength: "Yêu cầu ít hơn 30 kí tự",
+                              minLength: "Yêu cầu nhiều hơn 2 kí tự",
                             }}
                           />
                         </Col>
                       </Row>
 
                       <Row className="form-group">
-                        <Label htmlFor="doB" md={4}>
-                          {" "}Ngày sinh:{" "}
+                        <Label htmlFor="doB" md={5}>
+                          Ngày sinh
                         </Label>
-                        <Col md={8}>
+                        <Col md={7}>
                           <Control.text
                             model=".doB"
                             id="doB"
                             name="doB"
-                            placeholder="dd/mm/yyyy"
                             className="form-control"
+                            type="date"
                             validators={{
                               required,
                               minLength: minLength(1),
@@ -143,16 +165,17 @@ class Nhanvien extends Component {
                       </Row>
 
                       <Row className="form-group">
-                        <Label htmlFor="startDate" md={4}>
-                          {" "}Ngày vào công ty:{" "}
+                        <Label htmlFor="startDate" md={5}>
+                          Ngày vào công ty
                         </Label>
-                        <Col md={8}>
+                        <Col md={7}>
                           <Control.text
                             model=".startDate"
                             id="startDate"
                             name="startDate"
                             placeholder="dd/mm/yyyy"
                             className="form-control"
+                            type="date"
                             validators={{
                               required,
                               minLength: minLength(1),
@@ -171,12 +194,10 @@ class Nhanvien extends Component {
                       </Row>
 
                       <Row className="form-group">
-                        <Label htmlFor="sale" md={4}>
-                          {" "}
-                          Phòng ban:
-                          {" "}
+                        <Label htmlFor="sale" md={5}>
+                          Phòng ban
                         </Label>
-                        <Col md={8}>
+                        <Col md={7}>
                           <Control.select
                             model=".sale"
                             className="form-control"
@@ -194,53 +215,65 @@ class Nhanvien extends Component {
                           </Control.select>
                           <Errors
                             className="text-danger"
-                            model=".sale"
+                            model=".author"
                             show="touched"
+                            messages={{
+                              required: "Required",
+                            }}
                           />
                         </Col>
                       </Row>
 
                       <Row className="form-group">
-                        <Label htmlFor="salaryScale" md={4}>
-                          {" "}Hệ số lương:{" "}
+                        <Label htmlFor="salaryScale" md={5}>
+                          Hệ số lương
                         </Label>
-                        <Col md={8}>
+                        <Col md={7}>
                           <Control.text
                             model=".salaryScale"
                             id="salaryScale"
                             name="salaryScale"
-                            placeholder="1"
                             className="form-control"
+                            type="number"
+                            validators={{
+                              required,
+                            }}
                           />
                         </Col>
                       </Row>
 
                       <Row className="form-group">
-                        <Label htmlFor="numberOfStaff" md={4}>
-                          {" "}Số ngày nghỉ còn lại:{" "}
+                        <Label htmlFor="numberOfStaff" md={5}>
+                          Số ngày nghỉ còn lại
                         </Label>
-                        <Col md={8}>
+                        <Col md={7}>
                           <Control.text
                             model=".numberOfStaff"
                             id="numberOfStaff"
                             name="numberOfStaff"
-                            placeholder="0"
                             className="form-control"
+                            type="number"
+                            validators={{
+                              required,
+                            }}
                           />
                         </Col>
                       </Row>
 
                       <Row className="form-group">
-                        <Label htmlFor="annualLeave" md={4}>
-                          {" "}Số ngày đã làm thêm:{" "}
+                        <Label htmlFor="annualLeave" md={5}>
+                          Số ngày đã làm thêm
                         </Label>
-                        <Col md={8}>
+                        <Col md={7}>
                           <Control.text
                             model=".annualLeave"
                             id="annualLeave"
                             name="annualLeave"
-                            placeholder="0"
                             className="form-control"
+                            type="number"
+                            validators={{
+                              required,
+                            }}
                           />
                         </Col>
                       </Row>
@@ -249,23 +282,27 @@ class Nhanvien extends Component {
                       <Row className="form-group">
                         <Col>
                           <Button type="submit" color="primary">
-                            {" "}Thêm{" "}
+                            Thêm
                           </Button>
                         </Col>
                       </Row>
-
                     </LocalForm>
                   </ModalBody>
                 </Modal>
                 {/*************************End Button Add***************************/}
 
-                <form className="d-flex col-8 col-md-6 col-lg-4" onSubmit={this.handelSearch}>
-                  <input className="form-control me-2"
+                <form
+                  className="d-flex col-8 col-md-6 col-lg-4"
+                  onSubmit={this.handelSearch}
+                >
+                  <input
+                    className="form-control me-2"
                     type="search"
                     placeholder="Search..."
                     ref={this.input}
                   />
-                  <button className="btn btn-outline-dark"
+                  <button
+                    className="btn btn-outline-dark"
                     type="submit"
                     value="Submit"
                   >
@@ -285,4 +322,3 @@ class Nhanvien extends Component {
 }
 
 export default Nhanvien;
-
